8a0862986e95519b81eefe6f9388ea7c929fe0f5
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-31 10:19:31 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= tune_beats.length - 1; index++) {
-        radio.sendString("" + (colours[index]))
-        music.playTone(notes_freq[notes_name.indexOf(tune_notes[index])], tune_beats[index])
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index <= tune_beats.length - 1; index++) {
+            radio.sendString("" + (colours[index]))
+            music.playTone(notes_freq[notes_name.indexOf(tune_notes[index])], tune_beats[index])
+        }
+        basic.pause(500)
     }
 })
-let notes_name: string[] = []
 let notes_freq: number[] = []
+let notes_name: string[] = []
 let tune_beats: number[] = []
-let colours: string[] = []
 let tune_notes: string[] = []
+let colours: string[] = []
 basic.showLeds(`
     . . # # .
     . . # . .
@@ -17,8 +20,8 @@ basic.showLeds(`
     . # # . .
     `)
 radio.setGroup(1)
-tune_notes = ["C", "G", "F", "E", "D", "c", "G", "F", "E", "D", "c", "G", "F", "E", "F", "D"]
 colours = ["red", "green", "yellow", "red", "yellow", "green", "red", "green", "yellow", "green", "red", "green", "yellow", "green", "red", "yellow"]
+tune_notes = ["C", "G", "F", "E", "D", "c", "G", "F", "E", "D", "c", "G", "F", "E", "F", "D"]
 tune_beats = [1000, 1000, 250, 250, 250, 1000, 1000, 250, 250, 250, 1000, 1000, 250, 250, 250, 1000]
-notes_freq = [262, 294, 330, 349, 392, 440, 494, 523]
-notes_name = ["C", "D", "E", "F", "G", "A", "B", "c"]
+notes_name = ["1", "2", "3", "4", "5", "6", "7", "C", "D", "E", "F", "G", "A", "B", "c", "d", "e", "f", "g", "a"]
+notes_freq = [131, 147, 165, 175, 196, 220, 247, 262, 294, 330, 349, 392, 440, 494, 523, 587, 659, 698, 784, 880]
